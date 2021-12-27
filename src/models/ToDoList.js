fc73fe58717f24a245a8b40c63607d6b51245b03
Naty Ghanem <naty.ghanem@idcapture.fr>
2021-12-27 16:07:31 +0100

    feature: add mock and organize
    Signed-off-by: Naty Ghanem <naty.ghanem@idcapture.fr>

--- src/models/ToDoList.js
+++ src/models/ToDoList.js
@@ -17,22 +17,13 @@ class ToDoList {
 
 
     additem(item) {
-        console.log(this.items.length);
         if (this.itemVerfify()){
             if(!this.items.some(i => i.name === item.name)){
                 if(Math.floor((item.creationDate - this.getLastCreatedTime())/1000/60) > 30){
                     this.items.push(item);
-<<<<<<< Updated upstream
-                    if (this.items.length == 7) {
-                        return emailSenderService.sendEmail('hello', 'helloo');
-=======
+
                     if (this.items.length === 8) {
-<<<<<<< Updated upstream
-                        emailSenderService.sendEmail('hello', 'helloo');
-=======
                         return emailSenderService.sendEmail('hello', 'helloo');
->>>>>>> Stashed changes
->>>>>>> Stashed changes
                     }
                     return true
                 }
